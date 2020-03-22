67b58d93ee593df3dd6312ecdc96fdec8f9596b5
steve990 <steve@stevecodeguy.com>
2020-03-22 15:08:43 -0700

    Removed extra article tags

--- frontend/src/Routes/About/About.js
+++ frontend/src/Routes/About/About.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function About() {
     return (
-        <article>
+        <>
             <h1>Introduction</h1>
             <hr />
             <h3>Hi there!</h3>
             <p>My name is Steve Gilkes. <br />Welcome to my portfolio. <br />Please visit my site and view my projects. Contact me if you like what you see and would like to work with me!</p>
-        </article>
+        </>
     );
 }
--- frontend/src/Routes/ProjectPizzaStore/ProjectPizzaStore.js
+++ frontend/src/Routes/ProjectPizzaStore/ProjectPizzaStore.js
@@ -8,24 +8,20 @@ export default function ProjectPizzStore() {
         <div>
             <h1>Projects</h1>
             <hr />
-            <article>
-                <div className='projects'>
-                  <a href='https://pizzeria.bcitwebdeveloper.ca'>
-                      <img src={ pizza } alt='Pizza Slice' />
-                      <h4>See the Live Site!</h4>
-                  </a>
-                </div>
+            <div className='projects'>
+                <a href='https://pizzeria.bcitwebdeveloper.ca'>
+                    <img src={ pizza } alt='Pizza Slice' />
+                    <h4>See the Live Site!</h4>
+                </a>
+            </div>
 
-                <p>Group project to create a pizza store's online store using WordPress. It was required to have a web store for orders, Google maps, and user accounts.</p>
-                
-                <ProjectImage 
-                  imageData = {['Pizza', 'Pizza Store WebSite']}
-                />
+            <p>Group project to create a pizza store's online store using WordPress. It was required to have a web store for orders, Google maps, and user accounts.</p>
+            
+            <ProjectImage 
+                imageData = {['Pizza', 'Pizza Store WebSite']}
+            />
 
-                <p>My primary contribution was to the custom pizza generator which allows you to select igredients for your pie and watch as they get applied to the image for order. This function is fully responsive and will even recalculate if you rotate your phone.</p>
-
-
-            </article>
+            <p>My primary contribution was to the custom pizza generator which allows you to select igredients for your pie and watch as they get applied to the image for order. This function is fully responsive and will even recalculate if you rotate your phone.</p>
         </div>
     );
 }
